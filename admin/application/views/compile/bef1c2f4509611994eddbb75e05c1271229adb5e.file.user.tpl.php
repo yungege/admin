4f3506cb54738d2ba9fe64ef94b4ad8881a77e40
dayu <799318466@qq.com>
2017-08-23 14:24:53 +0800

    t

--- admin/application/views/compile/bef1c2f4509611994eddbb75e05c1271229adb5e.file.user.tpl.php
+++ admin/application/views/compile/bef1c2f4509611994eddbb75e05c1271229adb5e.file.user.tpl.php
@@ -1,13 +1,13 @@
-<?php /* Smarty version Smarty-3.1.13, created on 2017-08-23 14:21:29
+<?php /* Smarty version Smarty-3.1.13, created on 2017-08-23 14:24:17
          compiled from "/var/www/admin/admin/application/views/template/push/user.tpl" */ ?>
-<?php /*%%SmartyHeaderCode:2093145051599d1ee9d61484-67188121%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+<?php /*%%SmartyHeaderCode:1629111689599d1f919c56e3-35276752%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
   'file_dependency' => 
   array (
     'bef1c2f4509611994eddbb75e05c1271229adb5e' => 
     array (
       0 => '/var/www/admin/admin/application/views/template/push/user.tpl',
-      1 => 1503469268,
+      1 => 1503469300,
       2 => 'file',
     ),
     '1af1c7811d93168106c85becc3c13354fe96fe45' => 
@@ -17,7 +17,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
       2 => 'file',
     ),
   ),
-  'nocache_hash' => '2093145051599d1ee9d61484-67188121',
+  'nocache_hash' => '1629111689599d1f919c56e3-35276752',
   'function' => 
   array (
   ),
@@ -29,9 +29,9 @@ $_valid = $_smarty_tpl->decodeProperties(array (
   ),
   'has_nocache_code' => false,
   'version' => 'Smarty-3.1.13',
-  'unifunc' => 'content_599d1ee9e827e2_19421940',
+  'unifunc' => 'content_599d1f91a3e2c6_26479652',
 ),false); /*/%%SmartyHeaderCode%%*/?>
-<?php if ($_valid && !is_callable('content_599d1ee9e827e2_19421940')) {function content_599d1ee9e827e2_19421940($_smarty_tpl) {?><!DOCTYPE html>
+<?php if ($_valid && !is_callable('content_599d1f91a3e2c6_26479652')) {function content_599d1f91a3e2c6_26479652($_smarty_tpl) {?><!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -377,8 +377,6 @@ $_smarty_tpl->tpl_vars['tag']->value = (explode('-',$_smarty_tpl->tpl_vars['page
 
                     $.post('/push/puser', formdata, function(json){
 
-                        alert(11);
-alert(json.errCode);
                         if(json.errCode == 0){
                             alert('推送成功.');
                             window.location = '/push/user';
