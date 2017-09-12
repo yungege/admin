bd44da494a1d26624206fe9554d4c536d812491f
42290931@qq.com <42290931@qq.com>
2017-09-12 16:05:09 +0800

    t

--- admin/application/actions/user/Logout.php
+++ admin/application/actions/user/Logout.php
@@ -6,6 +6,9 @@ class LogoutAction extends BaseAction {
         session_destroy();
 
         $host = $_SERVER['SERVER_NAME'];
+        if($host == 'localhost'){
+            $host = $_SERVER['SERVER_ADDR'];
+        }
         setcookie(session_name(), '', time()-86400, '/', $host);
         setcookie('ttxs', '', time()-86400, '/', $host);
         header('Location:/login.html');
